0bf7baec908bdd339d86c49ae69cbf3d47902bdc
hbeaumont2204 <harrybsmith06@yahoo.co.uk>
2024-09-06 20:53:16 +0100

    6.9.24

--- Assets/Scripts/Input/playerMovement.cs
+++ Assets/Scripts/Input/playerMovement.cs
@@ -7,19 +7,37 @@ public class playerMovement : MonoBehaviour
 {
     PlayerInput playerInput;
     CharacterController characterController;
+    //Animator animator;
 
     Vector2 currentMovementInput;
     Vector3 currentMovement;
     bool isMovementPressed;
+    float rotationFactorPerFrame = 10.0f;
     
     void Awake()
     {
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
+        //animator = GetComponent<Animator>();
 
         playerInput.CharacterControls.Move.started += onMovementInput;
         playerInput.CharacterControls.Move.canceled += onMovementInput;
         playerInput.CharacterControls.Move.performed += onMovementInput;
+    }
+
+    void rotation()
+    {
+        Vector3 position;
+        position.x = currentMovement.x;
+        position.y = 0.0f;
+        position.z = currentMovement.z;
+        Quaternion currentRotation = transform.rotation;
+
+        if (isMovementPressed)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(position);
+            transform.rotation = Quaternion.Slerp(currentRotation, targetRotation, rotationFactorPerFrame * Time.deltaTime);
+        }
     }
 
     void onMovementInput (InputAction.CallbackContext context)
@@ -32,6 +50,8 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
+        //handleAnimation();
+        rotation();
         characterController.Move(currentMovement * Time.deltaTime);
     }
 
@@ -44,5 +64,21 @@ public class playerMovement : MonoBehaviour
     {
         playerInput.CharacterControls.Disable();
     }
+
+    /* void handleAnimation()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+
+        if (isMovementPressed && !isWalking)
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        else if (!isMovementPressed && isWalking)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    } */
 
 }
